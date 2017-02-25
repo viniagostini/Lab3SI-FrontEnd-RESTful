f5ca30ae1c24d327cd4ed8737f4ffde2b0232787
viniagostini <viniciusaagostini@gmail.com>
2017-02-24 23:38:58 -0300

    changes in app port

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.get('/', function(req, res){
     res.redirect('/index.html');
 });
 
-app.listen(9090);
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
